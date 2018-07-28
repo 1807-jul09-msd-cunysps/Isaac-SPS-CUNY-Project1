c643699db190973a6110f9f97d9303007794c3ab
Isaac Steiner <ilebwohlsteiner@gmail.com>
2018-07-28 10:11:13 -0400

    Added js to gulpfile and created basic navbar

--- RoboDexWebsite/gulpfile.js
+++ RoboDexWebsite/gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding />
+/// <binding BeforeBuild='default' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
@@ -10,11 +10,18 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function () {
     gulp.start("sass");
+    gulp.start("js");
 });
 
 gulp.task("sass", function () {
-    return gulp.src('./Content/bootstrap/bootstrap.scss')
+    return gulp.src('./Styles/scss/main.scss')
         .pipe(sass())
         .pipe(concat('main.css'))
         .pipe(gulp.dest('./Styles/css/'));
+});
+
+gulp.task("js", function () {
+    return gulp.src(['./Scripts/custom/custom.js', './Scripts/bootstrap.bundle.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./Scripts/custom/'));
 });
