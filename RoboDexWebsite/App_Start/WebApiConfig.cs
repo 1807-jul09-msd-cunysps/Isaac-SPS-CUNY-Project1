3b92d1ff494370dd7c351b79cce98f6761730a43
Isaac Steiner <ilebwohlsteiner@gmail.com>
2018-08-06 09:36:50 -0400

    Created new form

---
+++ RoboDexWebsite/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace RoboDexWebsite
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Allow cross-origin requests
+            config.EnableCors();
+
+            config.MapHttpAttributeRoutes();
+
+            // Makes JSON default
+            //config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
